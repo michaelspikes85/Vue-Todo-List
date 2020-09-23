090b7dc0e1de5910e64c8caf7b855bd1ec2c5c71
Michael Spikes <michael.spikes85@gmail.com>
2020-09-23 17:51:04 -0400

    Completed v1.0.0 of app

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,68 +1,39 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-    
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
-import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo'
-import axios from 'axios';
-
+import Header from "./components/layout/Header"
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data()  {
-    return{
-      todos: []
-    }
-  },
-  methods:  {
-    deleteTodo(id)  {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo)  {
-      this.todos = [...this.todos, newTodo];
-    }
-  },
-    created() {
-      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err));
-    }
+    Header
+  }
 }
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  body{
-    font-family: Arial, Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+#nav {
+  padding: 30px;
+}
 
-  .btn  {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
+#nav a {
+  font-weight: bold;
+  color: #eee;
+}
 
-  .btn:hover  {
-    background: #666;
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
